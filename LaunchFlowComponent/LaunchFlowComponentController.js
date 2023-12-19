9f2b05b7cfbe2ffa0187cd6075811d0beac1b119
isiis0406 <100381801+isiis0406@users.noreply.github.com>
2023-12-19 14:20:31 +0100

    Auto close window after flow run

--- LaunchFlowComponent/LaunchFlowComponentController.js
+++ LaunchFlowComponent/LaunchFlowComponentController.js
@@ -1,8 +1,15 @@
 ({
-    init : function (component) {
-    // Find the component whose aura:id is "flowData"
-    var flow = component.find("flowData");
-    // In that component, start your flow. Reference the flow’s Unique Name.
-    flow.startFlow("Button_d_ajout_d_un_compte_et_d_un_contact_via_Pappers");
+    init: function (component) {
+        // Find the component whose aura:id is "flowData"
+        var flow = component.find("flowData");
+        // In that component, start your flow. Reference the flow’s Unique Name.
+        flow.startFlow("Button_d_ajout_d_un_compte_et_d_un_contact_via_Pappers");
     },
-    })
+    handleFlowStatusChange : function (component, event, helper) {
+        // Vérifiez si le flux est terminé et fermez l'action rapide
+        if(event.getParam('status') === "FINISHED") {
+            // Ferme l'action rapide
+            $A.get("e.force:closeQuickAction").fire();
+        }
+    }
+})
